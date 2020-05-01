f44223b840efc8908c8e80a7befa534d12eeeeaa
Stanley Fils <stanleyfils@gmail.com>
2020-04-30 23:46:30 -0400

    Debug routes

--- routes/recipe.routes.js
+++ routes/recipe.routes.js
@@ -1,56 +1,58 @@
 // ROUTES FILE NEEDS TO BE REQUIRED IN THE APP.JS IN ORDER NOT TO GIVE 404
 // APP NEEDS TO KNOW YOU CREATED A NEW ROUTE FILE, THAT'S THE ONLY WAY FOR IT TO KNOW WHICH ROUTES YOU WANT TO HIT
 
-const express = require('express');
+const express = require("express");
 const recipeRouter = express.Router();
 
 // ********* require Recipe model in order to use it for CRUD *********
-const Recipe = require('../models/Recipe.model');
+const Recipe = require("../models/Recipe.model");
 
 // ****************************************************************************************
 // POST route to create a new recipe in the DB
 
 // *********<form action="/authors" method="POST">
-recipeRouter.post('/recipes', (req, res, next) => {
-	console.log(req.body);
-	Recipe.create(req.body).then((recipeDoc) => res.status(200).json(recipeDoc)).catch((err) => next(err));
+recipeRouter.post("/recipes", (req, res, next) => {
+  console.log(req.body);
+  Recipe.create(req.body)
+    .then((recipeDoc) => res.status(200).json(recipeDoc))
+    .catch((err) => next(err));
 });
 
 // ****************************************************************************************
 // GET all recipes from the DB
 
-recipeRouter.get('/recipes', (req, res, next) => {
-	Recipe.find() // <-- .find() method gives us always an ARRAY back
-		.then((recipesFromDB) => res.status(200).json({ recipes: recipesFromDB }))
-		.catch((err) => next(err));
+recipeRouter.get("/recipes", (req, res, next) => {
+  Recipe.find() // <-- .find() method gives us always an ARRAY back
+    .then((recipesFromDB) => res.status(200).json(recipesFromDB))
+    .catch((err) => next(err));
 });
 
 // ****************************************************************************************
 // POST route to delete the recipe
 
-recipeRouter.post('/recipes/:recipeId/delete', (req, res) => {
-	Recipe.findByIdAndRemove(req.params.recipeId)
-		.then(() => res.json({ message: 'Recipe successfully removed!' }))
-		.catch((err) => next(err));
+recipeRouter.post("/recipes/:recipeId/delete", (req, res) => {
+  Recipe.findByIdAndRemove(req.params.recipeId)
+    .then(() => res.json({ message: "Recipe successfully removed!" }))
+    .catch((err) => next(err));
 });
 
 // ****************************************************************************************
 // POST route to save the recipe updates
 
-recipeRouter.post('/recipes/:id/update', (req, res) => {
-	Recipe.findByIdAndUpdate(req.params.id, req.body, { new: true })
-		.then((updatedRecipe) => res.status(200).json({ recipe: updatedRecipe }))
-		.catch((err) => next(err));
+recipeRouter.post("/recipes/:id/update", (req, res) => {
+  Recipe.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then((updatedRecipe) => res.status(200).json(updatedRecipe))
+    .catch((err) => next(err));
 });
 
 // ****************************************************************************************
 // GET route for getting the recipe details
 
-recipeRouter.get('/recipes/:someRecipeId', (req, res) => {
-	Recipe.findById(req.params.someRecipeId)
-		.populate('recipe')
-		.then((foundRecipe) => res.status(200).json({ recipe: foundRecipe }))
-		.catch((err) => next(err));
+recipeRouter.get("/recipes/:someRecipeId", (req, res) => {
+  Recipe.findById(req.params.someRecipeId)
+    .populate("recipe")
+    .then((foundRecipe) => res.status(200).json(foundRecipe))
+    .catch((err) => next(err));
 });
 
 module.exports = recipeRouter;
--- routes/recipeBook.routes.js
+++ routes/recipeBook.routes.js
@@ -1,66 +1,66 @@
 // ROUTES FILE NEEDS TO BE REQUIRED IN THE APP.JS IN ORDER NOT TO GIVE 404
 // APP NEEDS TO KNOW YOU CREATED A NEW ROUTE FILE, THAT'S THE ONLY WAY FOR IT TO KNOW WHICH ROUTES YOU WANT TO HIT
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // ********* require Author and Book models in order to use them *********
 // const Recipe = require('../models/Recipe.model');
-const recipeBook = require('../models/RecipeBook.model');
+const recipeBook = require("../models/RecipeBook.model");
 
 // ****************************************************************************************
 // POST - create a book
 
 // <form action="/books" method="POST">
-router.post('/recipeBooks', (req, res) => {
-	// console.log(req.body);
-	recipeBook
-		.create(req.body)
-		.then((bookDoc) => res.status(200).json({ recipeBook: bookDoc }))
-		.catch((err) => next(err));
+router.post("/recipeBooks", (req, res) => {
+  // console.log(req.body);
+  recipeBook
+    .create(req.body)
+    .then((bookDoc) => res.status(200).json(bookDoc))
+    .catch((err) => next(err));
 });
 
 // ****************************************************************************************
 // GET route to get all the books
 
-router.get('/recipeBooks', (req, res) => {
-	recipeBook
-		.find()
-		.then((booksFromDB) => res.status(200).json({ recipeBook: booksFromDB }))
-		.catch((err) => next(err));
+router.get("/recipeBooks", (req, res) => {
+  recipeBook
+    .find()
+    .then((booksFromDB) => res.status(200).json(booksFromDB))
+    .catch((err) => next(err));
 });
 
 // ****************************************************************************************
 // POST route to delete the book
 
 // <form action="/books/{{this._id}}/delete" method="post">
-router.post('/recipeBooks/:recipeBookId/delete', (req, res) => {
-	recipeBook
-		.findByIdAndRemove(req.params.recipeBookId)
-		.then(() => res.json({ message: 'Successfully removed!' }))
-		.catch((err) => next(err));
+router.post("/recipeBooks/:recipeBookId/delete", (req, res) => {
+  recipeBook
+    .findByIdAndRemove(req.params.recipeBookId)
+    .then(() => res.json({ message: "Successfully removed!" }))
+    .catch((err) => next(err));
 });
 
 // ****************************************************************************************
 // POST route to save the updates
 
 // <form action="/books/{{foundBook._id}}/update" method="POST">
-router.post('/recipeBooks/:id/update', (req, res) => {
-	recipeBook
-		.findByIdAndUpdate(req.params.id, req.body, { new: true })
-		.then((updatedBook) => res.status(200).json({ recipeBook: updatedBook }))
-		.catch((err) => next(err));
+router.post("/recipeBooks/:id/update", (req, res) => {
+  recipeBook
+    .findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then((updatedBook) => res.status(200).json(updatedBook))
+    .catch((err) => next(err));
 });
 
 // ****************************************************************************************
 // GET route for getting the book details
 
-router.get('/recipeBooks/:someBookId', (req, res) => {
-	recipeBook
-		.findById(req.params.someBookId)
-		.populate('recipe')
-		.then((foundBook) => res.status(200).json({ recipeBook: foundBook }))
-		.catch((err) => next(err));
+router.get("/recipeBooks/:someBookId", (req, res) => {
+  recipeBook
+    .findById(req.params.someBookId)
+    .populate("recipe")
+    .then((foundBook) => res.status(200).json(foundBook))
+    .catch((err) => next(err));
 });
 
 module.exports = router;
